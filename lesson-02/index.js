f8089c926f132cd0e40764f59a7b4c1011abbe44
Александр Синяков <lexand41@yandex.ru>
2023-04-26 15:46:46 +0700

    initial

---
+++ lesson-02/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const item = 'table';
+const quantity = 2;
+const furniture = 'furniture'
+const price = 1400;
+const totalAmount = quantity * price;
+
+console.log(item);
+console.log(totalAmount);
+
---
+++ lesson_01/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const item = 'table';
+const quantity = 2;
+const furniture = 'furniture'
+const price = 1400;
+const totalAmount = quantity * price;
+
+console.log(item);
+console.log(totalAmount);
+
+
