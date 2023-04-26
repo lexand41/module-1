ce3d519881a4b0bcf5884350496cc5cf6bb5a670
Александр Синяков <lexand41@yandex.ru>
2023-04-26 16:20:50 +0700

    lesson_02

--- lesson-02/index.js
+++ lesson-02/index.js
@@ -1,11 +1,30 @@
 'use strict';
 
-const item = 'table';
-const quantity = 2;
-const furniture = 'furniture'
-const price = 1400;
-const totalAmount = quantity * price;
+{
+  const item = 'table';
+  const quantity = 2;
+  const products = 'furniture'
+  const price = 1400;
+  const totalAmount = quantity * price;
 
-console.log(item);
-console.log(totalAmount);
+  console.log(item);
+  console.log('Общая сумма товара:');
+  console.log(totalAmount);
+
+  console.log('Общая сумма товара: ${totalAmount}'); // почему в таком формате не видит переменную
+}
+
+{
+  const item = 'TV';
+  const quantity = 5;
+  const products = 'audio-video'
+  const price = 19000;
+  const totalAmount = quantity * price;
+
+  console.log(item);
+  console.log('Общая сумма товара:');
+  console.log(totalAmount);
+
+  console.log("Общая сумма товара: ${totalAmount}"); // почему в таком формате не видит переменную
+}
 
--- lesson_01/index.js
+++ lesson_01/index.js
@@ -2,11 +2,12 @@
 
 const item = 'table';
 const quantity = 2;
-const furniture = 'furniture'
+const products = 'furniture'
 const price = 1400;
 const totalAmount = quantity * price;
 
 console.log(item);
+console.log('Общая сумма товара:');
 console.log(totalAmount);
 
 
