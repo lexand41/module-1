26c1531887a22d9e96b0c59874ce62590a74a7b7
Александр Синяков <lexand41@yandex.ru>
2023-04-27 17:01:19 +0700

    lesson_3 Исправил

--- lesson_03/index.js
+++ lesson_03/index.js
@@ -1,6 +1,26 @@
 'use strict';
 
 {
+  const item = 'table';
+  const quantity = 2;
+  const products = 'furniture'
+  const price = 1200;
+  const totalAmount = quantity * price;
+
+  console.log(`Общая сумма: ${item} - ${totalAmount}`); 
+}
+
+{
+  const item = 'TV';
+  const quantity = 5;
+  const products = 'audio-video'
+  const price = 19000;
+  const totalAmount = quantity * price;
+
+  console.log(`Общая сумма: ${item} - ${totalAmount}`);
+}
+
+{
   const item = prompt('Наименование товара');
   const quantity = +prompt('Количество товара');
   const products = prompt('Категория товара');
