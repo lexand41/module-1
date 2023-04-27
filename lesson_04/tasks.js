9f14250be2ef69bb07e638a6c2ea31d23adecf14
Александр Синяков <lexand41@yandex.ru>
2023-04-27 14:40:12 +0700

    lesson_4, 3-я задача

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -1,9 +1,17 @@
 'use strict';
 
-const rain = Math.round(Math.random());
+// const rain = Math.round(Math.random());
 
-if (rain === 1){
-  console.log('Пошел дождь. Возьмите зонт');
-} else if (rain === 0){
-  console.log('Дождя нет');
-}
+// if (rain === 1){
+//   console.log('Пошел дождь. Возьмите зонт');
+// } else if (rain === 0){
+//   console.log('Дождя нет');
+// }
+
+
+const mathematics = +prompt('Введите кол-во баллов по математике:');
+const russianLanguage = +prompt('Введите кол-во баллов по русскому языку:');
+const informatics = +prompt('Введите кол-во баллов по информатике:');
+const examResults = mathematics + russianLanguage + informatics;
+
+console.log(examResults >= 265 ? 'Поздравляю, вы поступили на бюджет!' : 'Увы, вы не поступили на бюджет!');
