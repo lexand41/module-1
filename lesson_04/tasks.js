45046e7c721f24f8910813e3781c93227e1d4521
Александр Синяков <lexand41@yandex.ru>
2023-04-27 15:05:09 +0700

    lesson_4, 4-я задача

--- lesson_04/tasks.js
+++ lesson_04/tasks.js
@@ -9,9 +9,14 @@
 // }
 
 
-const mathematics = +prompt('Введите кол-во баллов по математике:');
-const russianLanguage = +prompt('Введите кол-во баллов по русскому языку:');
-const informatics = +prompt('Введите кол-во баллов по информатике:');
-const examResults = mathematics + russianLanguage + informatics;
+// const mathematics = +prompt('Введите кол-во баллов по математике:');
+// const russianLanguage = +prompt('Введите кол-во баллов по русскому языку:');
+// const informatics = +prompt('Введите кол-во баллов по информатике:');
+// const examResults = mathematics + russianLanguage + informatics;
 
-console.log(examResults >= 265 ? 'Поздравляю, вы поступили на бюджет!' : 'Увы, вы не поступили на бюджет!');
+// console.log(examResults >= 265 ? 'Поздравляю, вы поступили на бюджет!' : 'Увы, вы не поступили на бюджет!');
+
+
+const howMuchMoney = +prompt('Сколько денег вы хотите снять');
+
+console.log(howMuchMoney % 100 === 0 ? 'Вы можете снять данную сумму' : 'Вы не можете снять данную сумму, так как минимальный номинал купюр в банкомате - 100р');
