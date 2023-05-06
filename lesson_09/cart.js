489cd5f6764ce7fd4ebd595127756966012d04c0
lexand41 <lexand41@yandex.ru>
2023-05-06 15:47:10 +0700

    lesson09

---
+++ lesson_09/cart.js
@@ -0,0 +1,46 @@
+'use strict';
+
+
+const cart = {
+  items: [],
+  totalPrice: 0,
+  count: 0,
+
+  increaseCount(n) {
+    this.count += n; 
+  },
+
+  calculateItemPrice() {
+    this.totalPrice = this.items.reduce((sum, e) => sum + e.price * e.quant, 0);
+  },
+
+  add(id, price, quant=1) {
+    this.items.push({id: id, price: price, quant: quant});
+    this.increaseCount(quant);
+    this.calculateItemPrice();
+  },
+
+  getTotalPrice() {
+    return this.totalPrice;
+  },
+
+  clear() {
+    this.items = [];
+    this.totalPrice = 0;
+    this.count = 0;
+  },
+
+  print() {
+    console.log(JSON.stringify(this.items));
+    console.log(this.getTotalPrice());
+  },
+};
+
+cart.add('laptop', 400, 7);
+cart.add('tablet', 300, 4);
+cart.add('phone', 200, 1);
+cart.add('tv', 500, 3);
+
+// cart.clear();
+cart.print();
+
