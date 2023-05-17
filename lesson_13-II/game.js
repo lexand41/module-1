a19afa94ef970b5d7f91d8dd49f68524a99af32d
lexand41 <lexand41@yandex.ru>
2023-05-17 18:00:42 +0700

    lesson13-II

--- lesson_13-II/game.js
+++ lesson_13-II/game.js
@@ -1,7 +1,8 @@
 'use strict';
 
 (() => {
-  const FIGURES_RUS = ['камень', 'ножницы', 'бумага']
+  const FIGURES_RUS = ['камень', 'ножницы', 'бумага'];
+  const FIGURES_END = ['rock','scissors', 'paper'];
 
   const getRandomIntInclusive = (min, max) => {
     min = Math.ceil(min);
@@ -14,13 +15,17 @@
       player: 0,
       computer: 0,
     };
+
+    const lang = language === 'EN' || language === 'ENG' ?
+      FIGURES_END : FIGURES_RUS;
     
     return function start() {
-      let rock = 'камень';
-      let scissors = 'ножницы';
-      let paper = 'бумага';
-      const compStep = FIGURES_RUS[getRandomIntInclusive(0, 2)];
-      let userStep = prompt(FIGURES_RUS);
+      const rock = lang[0];
+      const scissors = lang[1];
+      const paper = lang[2];
+
+      const compStep = lang[getRandomIntInclusive(0, 2)];
+      let userStep = prompt(lang);
 
       if (userStep === null) {
         alert('Конец игры');
@@ -43,7 +48,7 @@
 
       const yesOrNo = () => {
         if (confirm('Ещё?') === false) {
-          confirm('Вы точно хотите выйти?"') === true ? alert('Конец игры') : start();
+          confirm('Вы точно хотите выйти?') === true ? alert('Конец игры') : start();
         } else {
           start();
         }
@@ -53,6 +58,9 @@
         case 'кк':
         case 'нн':
         case 'бб':
+        case 'rr':
+        case 'ss':
+        case 'pp':
           alert(`Компьютер: ${compStep}\nВы: ${userStep}\n  Ничья`);
           yesOrNo();
           break;
@@ -60,6 +68,9 @@
         case 'кн':
         case 'нб':
         case 'бк':
+        case 'rs':
+        case 'sp':
+        case 'pr':
           alert(`Компьютер: ${compStep}\nВы: ${userStep}\n  Вы победили!`);
           resault.player++;
           yesOrNo();
@@ -68,6 +79,9 @@
         case 'нк':
         case 'бн':
         case 'кб':
+        case 'sr':
+        case 'ps':
+        case 'rp':
           alert(`Компьютер: ${compStep}\nВы: ${userStep}\n  Вы проиграли`);
           resault.computer++;
           yesOrNo();
